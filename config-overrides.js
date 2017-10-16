1cd3fed898beca378836a2a6d402ab821156fced
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-10-17 00:33:32 +0300

    Add empty config-override.js

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+/**
+ * Override CRA webpack configs.
+ * @param config
+ * @param env
+ * @returns {*}
+ */
+
+module.exports = function override(config, env) {
+  //do stuff with the webpack config...
+  return config;
+};
