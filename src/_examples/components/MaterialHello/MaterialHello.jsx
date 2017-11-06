92303a10c7c28bfcac9458f563819ec84059b9f1
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-11-07 00:45:21 +0200

    Add material icons

--- src/_examples/components/MaterialHello/MaterialHello.jsx
+++ src/_examples/components/MaterialHello/MaterialHello.jsx
@@ -1,10 +1,19 @@
 // @flow
 import * as React from 'react';
 import TextField from 'material-ui/TextField';
+import Icon from 'material-ui/Icon';
+import AccessAlarmIcon from 'material-ui-icons/AccessAlarm';
+
 
 const MaterialHello = (): React.Node => (
   <div>
     <TextField label='Say hello' margin='normal' />
+    <br />
+    Font icon
+    <Icon color="primary">add_circle</Icon>
+    <br />
+    Svg icon
+    <AccessAlarmIcon color="primary" />
   </div>
 );
 
