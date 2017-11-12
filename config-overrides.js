53d34740fb369983f3cf470b12b9067cd58d1841
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-11-12 11:18:30 +0200

    Add cssnext

--- config-overrides.js
+++ config-overrides.js
@@ -7,6 +7,7 @@
 
 const { compose } = require('react-app-rewired');
 const rewireCSSModules = require('./config/rewire-css-modules');
+const rewirePostcss = require('./config/rewire-postcss');
 const rewireTypescript = require('./config/rewire-typescript');
 const rewireBabel = require('./config/rewire-babel');
 // const {inspect} = require('util');
@@ -14,6 +15,7 @@ const rewireBabel = require('./config/rewire-babel');
 
 module.exports = compose(
   rewireCSSModules,
+  rewirePostcss,
   rewireTypescript,
   rewireBabel
 );
