d8d03a6e9e08d75880e17898f890c07e49fa3720
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-11-12 23:19:34 +0200

    Remove cssnext

--- config-overrides.js
+++ config-overrides.js
@@ -7,19 +7,17 @@
 
 const { compose } = require('react-app-rewired');
 const rewireCSSModules = require('./config/rewire-css-modules');
-const rewirePostcss = require('./config/rewire-postcss');
 const rewireTypescript = require('./config/rewire-typescript');
 const rewireBabel = require('./config/rewire-babel');
 const rewireCssSourcemaps = require('./config/rewire-css-sourcemaps');
 const convertStringLoadersToObjects = require('./config/rewire-convert-string-loaders-to-objects');
-const debugRules = require('./config/rewire-debug-print-rules');
+// const debugRules = require('./config/rewire-debug-print-rules');
 
 module.exports = compose(
   // debugRules, // print rules and kill
 
   rewireCssSourcemaps, // should run after all css related rewiring!
   rewireCSSModules,
-  // rewirePostcss,
   rewireTypescript,
   rewireBabel,
   convertStringLoadersToObjects
