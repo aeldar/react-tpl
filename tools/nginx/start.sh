87051b0fe1c184ac4aea55f6b44fdb3673daeaf5
aeldar <eldar.aliyev8@gmail.com>
2017-12-05 00:23:19 +0200

    Fix interpolation of interpolation of interpolation

--- tools/nginx/start.sh
+++ tools/nginx/start.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+# Inject only env variables suited this mask
+MASK="WEB_APP_"
+
 MARKER='%%%ENV_JSON%%%'
 
 # welcome to backslash hell, there is no escaping here.
-ENV_JSON=$(sh ./env-to-json.sh WEB_APP_ | sed 's/\\/\\\\/g' | sed "s/'/\\\\\\\\'/g")
+ENV_JSON=$(sh ./env-to-json.sh $MASK | sed 's/\\/\\\\\\\\/g' | sed "s/'/\\\\\\\\'/g")
 
 echo $ENV_JSON
 
