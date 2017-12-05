abe6506d37d624bdd511eaa7a58ab783534c56d3
Eldar Aliyev <aeldar@users.noreply.github.com>
2017-12-05 14:22:15 +0200

    Update start.sh

--- tools/nginx/start.sh
+++ tools/nginx/start.sh
@@ -7,11 +7,9 @@ MASK="WEB_APP_"
 
 MARKER='%%%ENV_JSON%%%'
 
-# welcome to backslash hell, there is no escaping here.
+# welcome to backslash hell
 ENV_JSON=$(sh ./env-to-json.sh $MASK | sed 's/\\/\\\\\\\\/g' | sed "s/'/\\\\\\\\'/g")
 
-echo $ENV_JSON
-
-cat ./site.tpl.conf | sed "s/$MARKER/${ENV_JSON}/" > /etc/nginx/conf.d/default.conf
+sed "s/$MARKER/$ENV_JSON/" ./site.tpl.conf > /etc/nginx/conf.d/default.conf
 
 nginx -g 'daemon off;'
